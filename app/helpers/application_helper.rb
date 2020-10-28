07d8f1f324ef4b39354cd93b0546cf044f5b1ff6
Antonio Chavez <cavjzz@gmail.com>
2020-10-28 11:50:26 -0600

    Start Devise integration

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def alert_helper(text:, intent: 'primary')
+    tag.div(class: "alert alert-#{intent}", role: 'alert') do
+      tag.span text.html_safe
+
+      tag.button(type: 'button', class: 'close', 'data-dismiss' => 'alert', 'aria-label' => 'close') do
+        tag.span('&times;'.html_safe, 'aria-hidden' => true)
+      end
+    end
+  end
 end
