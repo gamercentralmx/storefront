9d6c127ba93febfdb6f172eebb3ec7b6243d2fd3
Antonio Chavez <cavjzz@gmail.com>
2020-10-28 11:59:49 -0600

    Add devise for Users

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,14 @@
 module ApplicationHelper
   def alert_helper(text:, intent: 'primary')
     tag.div(class: "alert alert-#{intent}", role: 'alert') do
-      tag.span text.html_safe
+      tag.div(class: 'container') do
+        content = tag.span text.html_safe
 
-      tag.button(type: 'button', class: 'close', 'data-dismiss' => 'alert', 'aria-label' => 'close') do
-        tag.span('&times;'.html_safe, 'aria-hidden' => true)
+        content += tag.button(type: 'button', class: 'close', 'data-dismiss' => 'alert', 'aria-label' => 'close') do
+          tag.span('&times;'.html_safe, 'aria-hidden' => true)
+        end
+
+        content
       end
     end
   end
