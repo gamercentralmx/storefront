2791d1042f3c3fd413db63d75d53a458827e537c
Antonio Chavez <cavjzz@gmail.com>
2020-10-31 11:18:54 -0600

    Start adding order processing

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,8 @@ class Product < ApplicationRecord
   belongs_to :category
   has_many :order_items, dependent: :destroy
 
+  delegate :name, to: :category, prefix: 'category'
+
   def specs
     metadata.map { |_, description| description }
   end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
 
     resources :categories
     resources :products
+    resources :orders
   end
 end
