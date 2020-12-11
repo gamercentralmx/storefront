a0e37db498dcf98319a4c0b7049228c49e43ffad
Antonio Chavez <cavjzz@gmail.com>
2020-12-11 15:01:14 -0600

    Update sorting

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,11 +2,9 @@ class CategoriesController < ApplicationController
   def show
     @category = Category.find_by!(slug: params[:id])
     @products = @category.products.published
+    @sorting = Product::SORTINGS['Menor Precio']
 
-    return unless params[:sort].present?
-
-    sorting = Product::SORTINGS[params[:sort]]
-
-    @products = @products.order(sorting)
+    @sorting = Product::SORTINGS[params[:sort]] if params[:sort].present?
+    @products = @products.order(@sorting)
   end
 end
