da277fd01e67c9b2b01759d5c9eea23fcbfa9dbe
Antonio Chavez <cavjzz@gmail.com>
2020-12-14 10:57:43 -0600

    Add cover picture for product

--- app/models/product.rb
+++ app/models/product.rb
@@ -12,6 +12,7 @@ class Product < ApplicationRecord
   delegate :name, :slug, to: :category, prefix: 'category'
 
   has_many_attached :pictures
+  has_one_attached :cover_picture
 
   before_save :parse_features
 
@@ -60,7 +61,8 @@ class Product < ApplicationRecord
       metadata: metadata,
       description: description,
       category_name: category_name,
-      pictures: pictures.map { |p| rails_blob_path(p, only_path: true) }
+      pictures: pictures.map { |p| rails_blob_path(p, only_path: true) },
+      cover_picture: cover_picture.present? ? rails_blob_path(cover_picture, only_path: true) : nil
     }
   end
 
